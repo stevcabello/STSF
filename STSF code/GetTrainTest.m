d641234455483fae6e25e6b7f78d61bdd97f5e2a
Steven Cabello <stevcabello@hotmail.com>
2020-09-24 15:58:04 +1000

    Add files via upload

---
+++ STSF code/GetTrainTest.m
@@ -0,0 +1,28 @@
+function [X_train, y_train, X_test, y_test] = GetTrainTest(str)
+
+    temp1 =what;dir0=temp1.path;dir1=[dir0 '/' 'datasetsUCR/'];
+
+    %uncomment this to change the datasets directory to your corresponding folder
+    % dir1=['/Users/ncabello/Documents/Univariate_arff/'];
+
+    str=char(str);
+    testDir= [dir1 str '/' str '_TEST.txt'];
+    trainDir= [dir1 str '/' str '_TRAIN.txt'];
+    TEST = load(testDir); 
+    TRAIN= load(trainDir);
+
+    X=TRAIN;X(:,1)=[];
+    y=TRAIN(:,1);
+
+    TEST1 = TEST;
+    clsTest=TEST1(:,1);
+    TEST1(:,1)=[];
+
+    X_train = X;
+    y_train = y;
+    X_test = TEST1;
+    y_test = clsTest;
+
+
+end
+
